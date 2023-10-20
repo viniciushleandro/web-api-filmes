ece8d497a05e3b3a044585c9767d2fd4279349ce
Vinicius <vinilean43@gmail.com>
2023-10-19 21:48:12 -0300

    Validações na requisição e retorno

--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -1,10 +1,17 @@
-﻿namespace FilmesApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
 
 public class Filme
 {
+    [Required(ErrorMessage ="O título do filme é obrigatório")]
     public string Titulo { get; set; }
 
+    [Required(ErrorMessage = "A duração do filme é obrigatório")]
+    [Range(70,600,ErrorMessage ="A duração do filme deve ser entre 70 à 600 minutos")]
     public int Duracao { get; set; }
 
+    [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
     public string Genero { get; set; }
 }
