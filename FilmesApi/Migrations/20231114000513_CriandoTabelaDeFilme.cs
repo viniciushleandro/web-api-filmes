745018df886aa7d8b78090cd9e486dd9aaa7fce9
Vinicius <vinilean43@gmail.com>
2023-11-13 21:31:22 -0300

    Salvando dados com Entity

--- FilmesApi/Migrations/20231114000513_CriandoTabelaDeFilme.cs
+++ FilmesApi/Migrations/20231114000513_CriandoTabelaDeFilme.cs
@@ -2,35 +2,34 @@
 
 #nullable disable
 
-namespace FilmesApi.Migrations
+namespace FilmesApi.Migrations;
+
+/// <inheritdoc />
+public partial class CriandoTabelaDeFilme : Migration
 {
     /// <inheritdoc />
-    public partial class CriandoTabelaDeFilme : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Filmes",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Duracao = table.Column<int>(type: "int", nullable: false),
-                    Genero = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Filmes", x => x.Id);
-                });
-        }
+        migrationBuilder.CreateTable(
+            name: "Filmes",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                Duracao = table.Column<int>(type: "int", nullable: false),
+                Genero = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Filmes", x => x.Id);
+            });
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "Filmes");
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "Filmes");
     }
 }
--- FilmesApi/Migrations/FilmeContextModelSnapshot.cs
+++ FilmesApi/Migrations/FilmeContextModelSnapshot.cs
@@ -7,45 +7,44 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace FilmesApi.Migrations
+namespace FilmesApi.Migrations;
+
+[DbContext(typeof(FilmeContext))]
+partial class FilmeContextModelSnapshot : ModelSnapshot
 {
-    [DbContext(typeof(FilmeContext))]
-    partial class FilmeContextModelSnapshot : ModelSnapshot
+    protected override void BuildModel(ModelBuilder modelBuilder)
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
-        {
 #pragma warning disable 612, 618
-            modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.13")
-                .HasAnnotation("Relational:MaxIdentifierLength", 128);
+        modelBuilder
+            .HasAnnotation("ProductVersion", "7.0.13")
+            .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
-            SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
+        SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
-            modelBuilder.Entity("FilmesApi.Models.Filme", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
+        modelBuilder.Entity("FilmesApi.Models.Filme", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
 
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+                SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
 
-                    b.Property<int>("Duracao")
-                        .HasColumnType("int");
+                b.Property<int>("Duracao")
+                    .HasColumnType("int");
 
-                    b.Property<string>("Genero")
-                        .IsRequired()
-                        .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
+                b.Property<string>("Genero")
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnType("nvarchar(50)");
 
-                    b.Property<string>("Titulo")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                b.Property<string>("Titulo")
+                    .IsRequired()
+                    .HasColumnType("nvarchar(max)");
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.ToTable("Filmes");
-                });
+                b.ToTable("Filmes");
+            });
 #pragma warning restore 612, 618
-        }
     }
 }
